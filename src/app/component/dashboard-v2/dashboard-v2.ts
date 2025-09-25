8ad59dfe98c8965da1cc68503289e70eec3d2ee2
pure <140476156+pure11pure@users.noreply.github.com>
2025-09-25 15:51:37 +0700

    add dashboardV2

--- src/app/component/dashboard-v2/dashboard-v2.ts
+++ src/app/component/dashboard-v2/dashboard-v2.ts
@@ -1,11 +1,72 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  Inject,
+  PLATFORM_ID,
+  OnInit,
+  ChangeDetectorRef,
+} from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard-v2',
-  imports: [],
+  standalone: true,
   templateUrl: './dashboard-v2.html',
-  styleUrl: './dashboard-v2.scss'
+  styleUrls: ['./dashboard-v2.scss'],
 })
-export class DashboardV2 {
+export class DashboardV2 implements OnInit {
+  svgContent: SafeHtml | null = null;
+
+  constructor(
+    private http: HttpClient,
+    private sanitizer: DomSanitizer,
+    private cdr: ChangeDetectorRef,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
+
+  async ngOnInit(): Promise<void> {
+    if (isPlatformBrowser(this.platformId)) {
+      await this.loadSvg();
+    }
+  }
+
+  async loadSvg() {
+    console.log('>> SVG Loaded _V2');
+    try {
+      const rawSvg = await firstValueFrom(
+        this.http.get('assets/halfCircle500.svg', { responseType: 'text' })
+      );
+
+      const parser = new DOMParser();
+      const svgDoc = parser.parseFromString(rawSvg, 'image/svg+xml');
+      const svg = svgDoc.documentElement;
+
+      svg.setAttribute('width', '1000');
+      svg.setAttribute('height', '800');
+
+      for (let i = 1; i <= 500; i++) {
+        const circle = svg.querySelector(`#circle-${i}`);
+        if (circle) {
+          let fill = 'gray';
+          if (i <= 120) fill = 'orange';
+          else if (i <= 260) fill = 'red';
+          else if (i <= 280) fill = 'blue';
+          circle.setAttribute('fill', fill);
+        }
+      }
+
+      const serializer = new XMLSerializer();
+      const modifiedSvg = serializer.serializeToString(svg);
+      this.svgContent = this.sanitizer.bypassSecurityTrustHtml(modifiedSvg);
+
+      setTimeout(() => {
+        this.cdr.markForCheck();
+      }, 0);
+    } catch (e) {
+      console.error('❌ SVG Load Error:', e);
+    }
+  }
 
 }

5da98a389fc97300d02a953c857775a78782e263
pure <140476156+pure11pure@users.noreply.github.com>
2025-10-08 14:21:50 +0700

    fix : ผังที่นั่ง

--- src/app/component/dashboard-v2/dashboard-v2.ts
+++ src/app/component/dashboard-v2/dashboard-v2.ts
@@ -112,9 +112,9 @@ export class DashboardV2 implements OnInit {
       svg.removeAttribute('height');
 
       svg.setAttribute('viewBox', '0 0 900 500');
-      // svg.style.margin = '20px 0';
+      svg.style.margin = '150px 0';
       svg.style.marginRight = '40px';
-      svg.style.marginTop = '60px';
+      // svg.style.marginTop = '220px';
       svg.style.height = '60vh';
 
       // 🧠 STEP 1: สร้าง flat list ของชื่อพรรค

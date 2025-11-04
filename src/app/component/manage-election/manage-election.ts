331d8e43e2de8fcdeefade395126c83e3d7abcab
pure <140476156+pure11pure@users.noreply.github.com>
2025-11-04 15:02:56 +0700

    fix : tab1

--- src/app/component/manage-election/manage-election.ts
+++ src/app/component/manage-election/manage-election.ts
@@ -1,28 +1,41 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
 import { MatTabsModule } from '@angular/material/tabs';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { A11yModule } from '@angular/cdk/a11y';
 
 @Component({
   selector: 'app-manage-election',
-  imports: [MatTabsModule, MatIconModule],
+  imports: [
+    MatTabsModule,
+    MatIconModule,
+    CommonModule,
+    MatSlideToggleModule,
+    A11yModule,
+  ],
   templateUrl: './manage-election.html',
   styleUrl: './manage-election.scss',
 })
 export class ManageElection {
   constructor(private router: Router) {}
+  menus: any;
 
   ngOnInit(): void {
-    const token = localStorage.getItem('currentUser'); // ตรวจ token
-    if (!token) {
-      // ถ้าไม่มี token → เด้งไป login
+    const storage = localStorage.getItem('currentUser');
+    if (!storage) {
       this.router.navigate(['/login']);
+    } else {
+      console.log('storage', JSON.parse(storage).MENULIST);
+      this.menus = JSON.parse(storage).MENULIST;
     }
   }
 
-  selectedIndex = 0; // track tab index
-
+  selectedIndex = 0;
   onTabChange(event: any) {
     this.selectedIndex = event.index;
   }
+
+  checked = false;
 }

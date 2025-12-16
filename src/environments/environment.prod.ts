d9602a05afe87c00d9c4be709c3621ad2d1c2e35
flukelnw1142 <73615503+flukelnw1142@users.noreply.github.com>
2025-12-16 11:34:44 +0700

    fix domain back

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  api_url: 'https://10.2.0.80:8000/api',
+  api_url: 'https://election.oneeclick.co:8000/api',
   // api_url: 'https://10.2.0.80:7057/api',
-  ws_color_url: 'wss://10.2.0.80:8000/api/Election/ws/color',
-  ws_results_url: 'wss://10.2.0.80:8000/api/Election/ws/results',
-  ws_summary_url: 'wss://10.2.0.80:8000/api/Election/ws/summary',
+  ws_color_url: 'wss://election.oneeclick.co:8000/api/Election/ws/color',
+  ws_results_url: 'wss://election.oneeclick.co:8000/api/Election/ws/results',
+  ws_summary_url: 'wss://election.oneeclick.co:8000/api/Election/ws/summary',
   name: 'production environment',
 };

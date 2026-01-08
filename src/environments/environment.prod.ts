0999bb451f952c7b6bbc3d0fe02462d8a7c30034
pure <140476156+pure11pure@users.noreply.github.com>
2026-01-08 11:09:33 +0700

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,4 +6,6 @@ export const environment = {
   ws_results_url: 'wss://election.oneeclick.co:8000/api/Election/ws/results',
   ws_summary_url: 'wss://election.oneeclick.co:8000/api/Election/ws/summary',
   name: 'production environment',
+  referendum_url: 'https://media.election.in.th/api/media/elections',
+  api_token: 'ers_RNpCQAy3AdPvDFYCUxl7iX64tXn0Xx4YN3kljkP8gpc'
 };

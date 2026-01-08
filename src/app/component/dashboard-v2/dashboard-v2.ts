cc89e22c0bfcc2cbc0af46208640cedc9cc69e50
pure <140476156+pure11pure@users.noreply.github.com>
2026-01-08 09:57:42 +0700

    fix : color dashboard2

--- src/app/component/dashboard-v2/dashboard-v2.ts
+++ src/app/component/dashboard-v2/dashboard-v2.ts
@@ -266,7 +266,13 @@ export class DashboardV2 implements OnInit {
   }
 
   getColor(winner: any): string {
-    const partyName = typeof winner === 'string' ? winner : winner?.party || '';
+    // const partyName = typeof winner === 'string' ? winner : winner?.party || '';
+
+    const rawName = typeof winner === 'string' ? winner : winner?.party || '';
+
+    const partyName = rawName
+      .replace(/^พรรค\s*/g, '')   // ✅ ตัดคำว่า "พรรค" ข้างหน้า
+      .trim();
     for (const keyword in this.partyColorMap) {
       if (partyName === this.partyColorMap[keyword].PARTY_NAME) {
         return this.partyColorMap[keyword].COLOR;
--- src/app/component/referendum-page/referendum-page.ts
+++ src/app/component/referendum-page/referendum-page.ts
@@ -29,12 +29,23 @@ export class ReferendumPage implements OnInit {
         const agreeVotes = q.options.find((o: any) => o.optionCode === ('agree'))?.totalVotes ?? 0;
         const disagreeVotes = q.options.find((o: any) => o.optionCode === ('disagree'))?.totalVotes ?? 0;
 
+        const agreePercent =
+          q.goodVotes > 0 ? +(agreeVotes / q.goodVotes * 100).toFixed(2) : 0;
+
+        const disagreePercent =
+          q.goodVotes > 0 ? +(disagreeVotes / q.goodVotes * 100).toFixed(2) : 0;
+
+        const diffPercent = Math.abs(agreePercent - disagreePercent);
+
         return {
           ...q,
           // agreePercent: ((agreeVotes / q.goodVotes) * 100).toFixed(2),
-          // disagreePercent: ((disagreeVotes / q.goodVotes) * 100).toFixed(2)
-          agreePercent: 75.00,
-          disagreePercent: 25.00
+          // disagreePercent: ((disagreeVotes / q.goodVotes) * 100).toFixed(2),
+          agreePercent,
+          disagreePercent,
+          showGuideLine: diffPercent <= 5
+          // agreePercent: 75.00,
+          // disagreePercent: 25.00
         };
       });
 

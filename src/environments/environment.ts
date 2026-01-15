7009f6c7bc1519a7ae70bd9aef4b2ffe92e3e39d
pure <140476156+pure11pure@users.noreply.github.com>
2026-01-15 09:58:44 +0700

    fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  api_url: 'http://127.0.0.1:8000/api',
+  api_url: 'https://127.0.0.1:8000/api',
   // api_url: 'https://localhost:7116/api',
   ws_color_url: 'wss://127.0.0.1:8000/api/Election/ws/color',
   ws_results_url: 'wss://127.0.0.1:8000/api/Election/ws/results',

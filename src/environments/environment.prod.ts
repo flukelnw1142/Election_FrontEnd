9d1a1aaea8b8793712b830efebc8f1781abe724b
flukelnw1142 <73615503+flukelnw1142@users.noreply.github.com>
2026-01-29 14:20:05 +0700

    fix env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
-  api_url: 'https://election.oneeclick.co:8000/api',
+  api_url: 'https://oneelection.oneeclick.co:8000/api',
   // api_url: 'https://10.2.0.80:7057/api',
-  ws_color_url: 'wss://election.oneeclick.co:8000/api/Election/ws/color',
-  ws_results_url: 'wss://election.oneeclick.co:8000/api/Election/ws/results',
-  ws_summary_url: 'wss://election.oneeclick.co:8000/api/Election/ws/summary',
-  ws_ectreport_url: 'wss://election.oneeclick.co:8000/api/Election/ws/ectreport',
+  ws_color_url: 'wss://oneelection.oneeclick.co:8000/api/Election/ws/color',
+  ws_results_url: 'wss://oneelection.oneeclick.co:8000/api/Election/ws/results',
+  ws_summary_url: 'wss://oneelection.oneeclick.co:8000/api/Election/ws/summary',
+  ws_ectreport_url: 'wss://oneelection.oneeclick.co:8000/api/Election/ws/ectreport',
   name: 'production environment',
 };

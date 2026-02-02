a2a389886a4eac5388f0f3cb7b5367f6ea13dda3
pure <140476156+pure11pure@users.noreply.github.com>
2026-02-02 17:28:09 +0700

    fix

--- src/app/component/dashboard/dashboard.ts
+++ src/app/component/dashboard/dashboard.ts
@@ -22,7 +22,8 @@ import {
   Subscription,
   takeUntil,
   timeout,
-  map
+  map,
+  tap
 } from 'rxjs';
 import { isPlatformBrowser } from '@angular/common';
 import * as d3 from 'd3';
@@ -239,10 +240,30 @@ export class Dashboard implements OnInit {
     if (!isPlatformBrowser(this.platformId)) return;
     this.loadingSubject.next(true);
     this.updateFooterVisibility();
+    // this.filteredProvinces_Specific = this.provinceCtrl_Specific.valueChanges.pipe(
+    //   startWith(''),
+    //   map((value) => this._filterProvince(value || ''))
+    // );
     this.filteredProvinces_Specific = this.provinceCtrl_Specific.valueChanges.pipe(
       startWith(''),
-      map((value) => this._filterProvince(value || ''))
+      map(value => value || ''),
+      tap(value => {
+        const input = value.trim();
+
+        if (!input) {
+          this.selectedProvince_Specific = '';
+          return;
+        }
+
+        const matched = this.provinces.find(
+          p => p.provinceName === input
+        );
+
+        this.selectedProvince_Specific = matched ? matched.provinceName : '';
+      }),
+      map(value => this._filterProvince(value))
     );
+
     this.getProvince();
 
     try {
@@ -1969,7 +1990,7 @@ export class Dashboard implements OnInit {
     svg.style.margin = '0 auto';
     if (this.isDesktopOnly()) {
       svg.style.height = '68vh';
-    }else {
+    } else {
 
       svg.style.height = '40vh';
     }
@@ -2379,9 +2400,11 @@ export class Dashboard implements OnInit {
   }
 
   onSubmitFilter_Specific() {
+    console.log(this.provinceCtrl_Specific.value);
+
     if (!this.selectedProvince_Specific) {
       this.sweetAlertService.showAlert(
-        'Load Fail',
+        'แจ้งเตือน',
         'กรุณาเลือกจังหวัด',
         'warning'
       );
